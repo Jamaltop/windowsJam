0ce7ea1573a0958e71ba84cb46e83ad2e34eafad
Camal <camalkos91@gmail.com>
2025-04-15 12:18:58 +0400

    google

--- js/google.js
+++ js/google.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
       if (userData) {
         const user = JSON.parse(userData);
         document.getElementById("user-info").innerHTML = `
-          <div class="user-logo" style="width: 30px; height: 30px; display: flex; justify-content: center; align-items: center;">
+          <div class="user-logo" style="width: 30px; height: 30px; display: flex; justify-content: center; align-items: end; padding: 3px">
             <svg xmlns="http://www.w3.org/2000/svg" width="20px" viewBox="0 0 448 512">
               <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"/>
             </svg>
